c3be2cf290d09a1ee284f94eea1827acf25a7fe6
tiendat3011 <100399355+tiendat3011@users.noreply.github.com>
2022-04-21 20:48:20 +0700

    first commit

--- Module2/src/_16_io_text_file/exercise/read_file_csv/MainCountry.java
+++ Module2/src/_16_io_text_file/exercise/read_file_csv/MainCountry.java
@@ -9,7 +9,7 @@ public class MainCountry {
     public static List<Country> countryList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        List<String[]> stringList = Read.readFile("Module2\\src\\_16_io_text_file\\exercise\\read_file_csv\\countries.csv");
+        List<String[]> stringList = Read.readFile("Module2\\src\\_16_io_text_file\\exercise\\read_file_csv\\Countries.csv");
         for (String[] item : stringList
         ) {
        Country country = new Country(Integer.parseInt(item[0]),item[1],item[2]);
